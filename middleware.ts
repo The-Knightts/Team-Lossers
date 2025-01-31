9b8e0b6a96603241d7782fa62bb012bb04b82d72
The Knights <145095594+The-Knightts@users.noreply.github.com>
2025-01-31 15:44:49 +0530

    More Changes

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,10 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-const isProtectedRoute = createRouteMatcher(['/dashboard(.*)', '/'])
-
 export default clerkMiddleware(async (auth, req) => {
   if (isProtectedRoute(req)) await auth.protect()
 })
+
+const isProtectedRoute = createRouteMatcher(['/dashboard(.*)', '/'])
 
 export const config = {
   matcher: [
@@ -13,4 +13,4 @@ export const config = {
     // Always run for API routes
     '/(api|trpc)(.*)',
   ],
-}
+};
